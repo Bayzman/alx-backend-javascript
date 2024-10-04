3190af67359907a757a37640053489d538de18e7
bayzman <bassmaninno102@gmail.com>
2024-10-04 05:51:01 +0300

    advanced tasks

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,8 @@
+export default function createIteratorObject(report) {
+    const totalEmployee = [];
+    for (const idx of Object.values(report.allEmployees)) {
+        totalEmployee.push(...idx);
+    }
+
+    return totalEmployee;
+}
