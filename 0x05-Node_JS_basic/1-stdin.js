d55bd4c1756f4182e08e58046cdb9b2e9a35273b
bayzman <bassmaninno102@gmail.com>
2024-11-27 07:47:21 +0300

    Using Process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,10 +1,5 @@
 #!/usr/bin/node
 
-/**
- * Creates a program that will be executed through
- * the command line
- */
-
 console.log('Welcome to Holberton School, what is your name?')
 process.stdin.on('data', (data) => {
   if (data != null) {
