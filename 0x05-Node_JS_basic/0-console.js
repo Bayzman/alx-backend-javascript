4dc0e855c8f771a8a482a31947f238d05f2be1df
bayzman <bassmaninno102@gmail.com>
2024-11-27 07:30:55 +0300

    Executing basic javascript with Node JS

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,11 @@
+/**
+ * Prints a message to the console
+ * 
+ * @param {string} args - The message to print
+ */
+
+module.exports = displayMessage;
+
+function displayMessage(args) {
+    console.log(args);
+}
