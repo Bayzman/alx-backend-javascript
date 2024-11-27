d96524a1d8555e4e36ecd3fb7867487d0d2dd99b
bayzman <bassmaninno102@gmail.com>
2024-11-27 07:43:37 +0300

    Using Process stdin

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+/**
+ * Creates a program that will be executed through
+ * the command line
+ */
+
+console.log('Welcome to Holberton School, what is your name?')
+process.stdin.on('data', (data) => {
+    process.stdout.write('Your name is: ' + data);
+})
+
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+  process.exit();
+});
