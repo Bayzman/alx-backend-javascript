c7cb8a51f708efc970cca2b44532453d9a2f229e
bayzman <bassmaninno102@gmail.com>
2024-11-27 07:51:08 +0300

    Using Process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -8,7 +8,7 @@
 console.log('Welcome to Holberton School, what is your name?')
 process.stdin.on('data', (data) => {
   if (data != null) {
-    process.stdout.write(`Your name is: ' ${data}`);
+    process.stdout.write(`Your name is: ${data}`);
   }
 });
 
